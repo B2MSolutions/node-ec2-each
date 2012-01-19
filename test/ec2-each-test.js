4e75fe7def64946045d6d6b381eaa791b8b14092
B2M Development <development@b2m-solutions.com>
2012-01-19 15:08:37 +0000

    added vows tests for validation of configuration upon construction

--- test/ec2-each-test.js
+++ test/ec2-each-test.js
@@ -15,7 +15,59 @@ var should = require('should'),
 
 var EC2 = ec2.EC2;
 
-vows.describe('ec2-each').addBatch({
+vows.describe('ec2-each')
+.addBatch({
+  'when configuration is undefined' : {
+    topic: undefined,
+    'should throw when creating EC2': function(config) {
+      (function(){
+        new EC2(config);
+      }).should.throw('Missing configuration');
+    }
+  },
+  'when configuration is null' : {
+    topic: null,
+    'should throw when creating EC2': function(config) {
+      (function(){
+        new EC2(config);
+      }).should.throw('Missing configuration');
+    }
+  },
+  'when configuration is missing access key Id' : {
+    topic: { secretAccessKey: "s", awsAccountId: "1", region:"r"},
+    'should throw when creating EC2': function(config) {      
+      (function() {
+        new EC2(config);
+      }).should.throw('missing AWS access key Id');
+    }
+  },
+  'when configuration is missing secretAccessKey' : {
+    topic: { accessKeyId: "x", awsAccountId: "1", region:"r"},
+    'should throw when creating EC2': function(config) {      
+      (function() {
+        new EC2(config);
+      }).should.throw('missing AWS secret access key');
+    }
+  },
+  'when configuration is missing account Id' : {
+    topic: { accessKeyId: "x", secretAccessKey: "s", region:"r"},
+    'should throw when creating EC2': function(config) {      
+      (function() {
+        new EC2(config);
+      }).should.throw('missing AWS account Id');
+    }
+  },
+  'when configuration is missing region' : {
+    topic: { accessKeyId: "x", secretAccessKey: "s", awsAccountId: "1"},
+    'should throw when creating EC2': function(config) {      
+      (function() {
+        new EC2(config);
+      }).should.throw('missing AWS region');
+    }
+  },
+})
+/*
+.addBatch({
   'An EC2' : {
     topic: new EC2(),
       'with invalid configuration' : {
@@ -33,4 +85,4 @@ vows.describe('ec2-each').addBatch({
         }
       }
   }
-}).export(module);
+})*/.export(module);
